825e9f09340483d8c3107ff6ac59e51105e3b212
turdaliyevjoraqozi <turdaliyevjoraqozi@gmail.com>
2022-05-12 07:51:07 +0500

    Created privateRout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import store from "./store";
+// import store from "./store";
 // import { createStore } from "redux";
 // import reducer from "./redux/reducer";
 // import { inc } from "./redux/actions";
@@ -14,7 +15,9 @@ import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <BrowserRouter>
+    {/* <Provider store={store}> */}
     <App />
-  </Provider>
+    {/* </Provider> */}
+  </BrowserRouter>
 );
